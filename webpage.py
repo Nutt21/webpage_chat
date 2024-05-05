20c0993b7a572c13b812c2ea6a0779280f09b18d
Nitheeshwaran R <87924073+Nutt21@users.noreply.github.com>
2024-05-05 14:59:20 +0530

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -22,15 +22,21 @@ def webpage_chat(question, url, openai_api_key):
     # Create a vector store, here I'm using chromadb
     persist_directory = 'docs/chroma/'
     vectordb = Chroma.from_documents(documents=docs, embedding=embedding, persist_directory=persist_directory)
+    
+    # Similarity search
+    context = vectordb.similarity_search(question,k=3)
+    # context_1 = "\n".join(result["text"] for result in context_results)
+    print(context)
 
     # Building prompt template
     template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
     {context}
     Question: {question}
     Helpful Answer:"""
+    
     QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
-    # Initialize ChatOpenAI for question answering
+    # Initialize ChatOpenAI for question-answering
     llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
 
     # Initialize question-answering chain
